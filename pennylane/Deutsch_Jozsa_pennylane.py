d5a5323b32da1fefced9140f6f3fad1d67d46681
Marzio Vallero <marziovallero@gmail.com>
2022-04-13 15:24:21 +0000

    Add pennylane generator for dj

--- pennylane/Deutsch_Jozsa_pennylane.py
+++ pennylane/Deutsch_Jozsa_pennylane.py
@@ -1,58 +1,42 @@
 # importing Qiskit
-from qiskit import QuantumCircuit, assemble, transpile
-
-
+import pennylane as qml
 
 def build_circuit(n, b_str):
     # set the length of the n-bit input string. 
     #n = 9
     
-    
-    balanced_oracle = QuantumCircuit(n+1)
-    #b_str = "101101101"
-    
-    # Place X-gates
-    for qubit in range(len(b_str)):
-        if b_str[qubit] == '1':
-            balanced_oracle.x(qubit)
-    
-    # Use barrier as divider
-    balanced_oracle.barrier()
-    
-    # Controlled-NOT gates
-    for qubit in range(n):
-        balanced_oracle.cx(qubit, n)
-    
-    balanced_oracle.barrier()
-    
-    # Place X-gates
-    for qubit in range(len(b_str)):
-        if b_str[qubit] == '1':
-            balanced_oracle.x(qubit)
-    
-    
-    dj_circuit = QuantumCircuit(n+1, n)
-    
-    # Apply H-gates
-    for qubit in range(n):
-        dj_circuit.h(qubit)
-    
-    # Put qubit in state |->
-    dj_circuit.x(n)
-    dj_circuit.h(n)
-    
-    # Add oracle
-    #dj_circuit += balanced_oracle
-    dj_circuit.compose(balanced_oracle, inplace=True)
-    
-    # Repeat H-gates
-    for qubit in range(n):
-        dj_circuit.h(qubit)
-    dj_circuit.barrier()
-    
-    # Measure
-    for i in range(n):
-        dj_circuit.measure(i, i)
-    
-    return dj_circuit
+    # We need a circuit with n qubits, plus one auxiliary qubit
+    dev = qml.device("default.qubit", wires=n+1)
 
+    @qml.qnode(dev)
+    def circuit():        
+        # Apply H-gates
+        for qubit in range(n):
+            qml.Hadamard(wires=qubit)
+        
+        # Put qubit in state |->
+        qml.PauliX(wires=n)
+        qml.Hadamard(wires=n)
+        
+        # Add oracle
+        # Place X-gates
+        for qubit in range(len(b_str)):
+            if b_str[qubit] == '1':
+                qml.PauliX(wires=qubit)
+        
+        # Controlled-NOT gates
+        for qubit in range(n):
+            qml.CNOT(wires=[qubit, n])
+                
+        # Place X-gates
+        for qubit in range(len(b_str)):
+            if b_str[qubit] == '1':
+                qml.PauliX(wires=qubit)
+        
+        # Repeat H-gates
+        for qubit in range(n):
+            qml.Hadamard(wires=qubit)
+        
+        return qml.probs(wires=range(n))
+
+    return circuit
