67144a5a656f6fe119be9b01455320939e578439
Marzio Vallero <marziovallero@gmail.com>
2022-04-13 15:13:37 +0000

    Add pennylane generator for bv

--- pennylane/Bernstein_Vazirani_pennylane.py
+++ pennylane/Bernstein_Vazirani_pennylane.py
@@ -1,45 +1,36 @@
 # importing Qiskit
-from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, transpile, assemble
-
+import pennylane as qml
 
 def build_circuit(n, s):
     #n = 9 # number of qubits used to represent s
     #s = '011011011'   # the hidden binary string
     
-    #%%
     # We need a circuit with n qubits, plus one auxiliary qubit
-    # Also need n classical bits to write the output to
-    bv_circuit = QuantumCircuit(n+1, n)
-    
-    # put auxiliary in state |->
-    bv_circuit.h(n)
-    bv_circuit.z(n)
-    
-    # Apply Hadamard gates before querying the oracle
-    for i in range(n):
-        bv_circuit.h(i)
-        
-    # Apply barrier 
-    bv_circuit.barrier()
-    
-    # Apply the inner-product oracle
-    s = s[::-1] # reverse s to fit qiskit's qubit ordering
-    for q in range(n):
-        if s[q] == '0':
-            bv_circuit.i(q)
-        else:
-            bv_circuit.cx(q, n)
-            
-    # Apply barrier 
-    bv_circuit.barrier()
-    
-    #Apply Hadamard gates after querying the oracle
-    for i in range(n):
-        bv_circuit.h(i)
-    
-    # Measurement
-    for i in range(n):
-        bv_circuit.measure(i, i)
+    dev = qml.device("default.qubit", wires=n+1)
 
-    return bv_circuit
+    @qml.qnode(dev)
+    def circuit():
+        # put auxiliary in state |->
+        qml.Hadamard(wires=n)
+        qml.PauliZ(wires=n)
+
+        # Apply Hadamard gates before querying the oracle
+        for i in range(n):
+            qml.Hadamard(wires=i)
+        
+        # Apply the inner-product oracle
+        si = s[::-1] # reverse s to fit qiskit's qubit ordering
+        for q in range(n):
+            if si[q] == '0':
+                qml.Identity(wires=q)
+            else:
+                qml.CNOT(wires=[q, n])
+        
+        #Apply Hadamard gates after querying the oracle
+        for i in range(n):
+            qml.Hadamard(wires=i)
+
+        return qml.probs(wires=range(n))
+
+    return circuit
 
