8f56444e44b54fd25aff340defe50fd2c4f74269
Marzio Vallero <marziovallero@gmail.com>
2022-04-13 15:35:06 +0000

    Add pennylane generator for grover

--- pennylane/Grover_pennylane.py
+++ pennylane/Grover_pennylane.py
@@ -1,30 +1,28 @@
 # Importing standard Qiskit libraries
-from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile, Aer, IBMQ, execute
-from qiskit.tools.jupyter import *
-from qiskit.visualization import *
+import pennylane as qml
 
 def build_circuit():
-    qreg_q = QuantumRegister(2, 'q')
-    creg_c = ClassicalRegister(2, 'c')
-    circuit = QuantumCircuit(qreg_q, creg_c)
-    
-    circuit.h(qreg_q[0])
-    circuit.h(qreg_q[1])
-    circuit.h(qreg_q[1])
-    circuit.cx(qreg_q[0], qreg_q[1])
-    circuit.h(qreg_q[0])
-    circuit.h(qreg_q[1])
-    circuit.x(qreg_q[0])
-    circuit.h(qreg_q[1])
-    circuit.x(qreg_q[1])
-    circuit.h(qreg_q[1])
-    circuit.cx(qreg_q[0], qreg_q[1])
-    circuit.x(qreg_q[0])
-    circuit.h(qreg_q[1])
-    circuit.h(qreg_q[0])
-    circuit.x(qreg_q[1])
-    circuit.h(qreg_q[1])
-    circuit.measure(qreg_q[0], creg_c[0])
-    circuit.measure(qreg_q[1], creg_c[1])
-    return circuit
+    dev = qml.device("default.qubit", wires=2)
 
+    @qml.qnode(dev)
+    def circuit():
+        qml.Hadamard(wires=0)
+        qml.Hadamard(wires=1)
+        qml.Hadamard(wires=1)
+        qml.CNOT(wires=[0, 1])
+        qml.Hadamard(wires=0)
+        qml.Hadamard(wires=1)
+        qml.PauliX(wires=0)
+        qml.Hadamard(wires=1)
+        qml.PauliX(wires=1)
+        qml.Hadamard(wires=1)
+        qml.CNOT(wires=[0, 1])
+        qml.PauliX(wires=0)
+        qml.Hadamard(wires=1)
+        qml.Hadamard(wires=0)
+        qml.PauliX(wires=1)
+        qml.Hadamard(wires=1)
+        
+        return qml.probs(wires=range(2))
+
+    return circuit
