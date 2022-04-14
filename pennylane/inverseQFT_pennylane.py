acada7f7e9677c74e892e3ba6fca058e807cb2c2
Marzio Vallero <marziovallero@gmail.com>
2022-04-14 07:35:29 +0000

    Add pennylane generator for iqft

--- pennylane/inverseQFT_pennylane.py
+++ pennylane/inverseQFT_pennylane.py
@@ -1,55 +1,45 @@
-from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+import pennylane as qml
 from numpy import pi
 import math
 
-def qft_rotations(circuit, n):
+def qft_rotations(wires):
     """Performs qft on the first n qubits in circuit (without swaps)"""
-    if n == 0:
-        return circuit
-    n -= 1
-    circuit.h(n)
-    for qubit in range(n):
-        circuit.cp(pi/2**(n-qubit), qubit, n)
+    if wires == 0:
+        return
+    wires -= 1
+    qml.Hadamard(wires=wires)
+    for qubit in range(wires):
+        qml.ControlledPhase(pi/2**(wires-qubit), wires=[qubit, wires])
     # At the end of our function, we call the same function again on
     # the next qubits (we reduced n by one earlier in the function)
-    qft_rotations(circuit, n)
+    qft_rotations(wires)
     
-def swap_registers(circuit, n):
-    for qubit in range(n//2):
-        circuit.swap(qubit, n-qubit-1)
-    return circuit
+def swap_registers(wires):
+    for qubit in range(wires//2):
+        qml.SWAP(wires=[qubit, wires-qubit-1])
 
-def qft(circuit, n):
+def qft(wires):
     """QFT on the first n qubits in circuit"""
-    qft_rotations(circuit, n)
-    swap_registers(circuit, n)
-    return circuit
-
-def inverse_qft(circuit, n):
-    """Does the inverse QFT on the first n qubits in circuit"""
-    # First we create a QFT circuit of the correct size:
-    qft_circ = qft(QuantumCircuit(n), n)
-    # Then we take the inverse of this circuit
-    invqft_circ = qft_circ.inverse()
-    # And add it to the first n qubits in our existing circuit
-    circuit.append(invqft_circ, circuit.qubits[:n])
-    return circuit.decompose() # .decompose() allows us to see the individual gates
+    qft_rotations(wires)
+    swap_registers(wires)
 
 def build_circuit(nqubits):
     number = '10'*math.ceil(nqubits/2)
     number = int(number[0:nqubits], 2) # code a number interleaving 1 and 0 in binary
-    powerTwo = 2**(nqubits-1)
-    qc = QuantumCircuit(nqubits,nqubits)
-    for qubit in range(nqubits):
-        qc.h(qubit)
-        qc.p(number*pi/powerTwo,qubit)
-        powerTwo = powerTwo/2
-    qc = inverse_qft(qc, nqubits)
-    #qc.measure_all()
-    # Measurement
-    qc.barrier()
-    for i in range(nqubits):
-        print(i)
-        qc.measure(i, i)
-    return qc
 
+    dev = qml.device("default.qubit", wires=nqubits)
+
+    @qml.qnode(dev)
+    def circuit():
+        powerTwo = 2**(nqubits-1)
+        for qubit in range(nqubits):
+            qml.Hadamard(qubit)
+            qml.PhaseShift(phi=number*pi/powerTwo, wires=qubit)
+            powerTwo = powerTwo/2
+        #qml.QFT(wires=range(nqubits)).inv().decomposition()
+        qml.adjoint(qft)(nqubits)
+
+        # Measurement
+        return qml.probs(wires=range(nqubits))
+
+    return circuit
