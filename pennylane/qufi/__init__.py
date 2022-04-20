4c2f6ecf6bc9ed83e5092bb7a51865bc998417fe
Marzio <marziovallero@gmail.com>
2022-04-20 16:40:41 +0200

    Add Bell circuit for debugging purposes

--- pennylane/qufi/__init__.py
+++ pennylane/qufi/__init__.py
@@ -3,4 +3,5 @@ from qufi.circuit_generator import BernsteinVazirani
 from qufi.circuit_generator import DeutschJozsa
 from qufi.circuit_generator import Grover
 from qufi.circuit_generator import IQFT
+from qufi.circuit_generator import Bell
 from qufi.results_processing import *
