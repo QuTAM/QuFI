1508fd6fd3b5de8c3c4b5eb34b1893e5c9a3d43a
marziovallero@gmail.com <marziovallero@gmail.com>
2022-04-30 17:06:31 +0000

    Update according to new methods

--- pennylane/run_circuits.py
+++ pennylane/run_circuits.py
@@ -1,7 +1,7 @@
 #%%
 import numpy as np
 from qiskit.test.mock import FakeSantiago
-from qufi import execute_over_range, BernsteinVazirani, Bell
+from qufi import execute_over_range, BernsteinVazirani, get_qiskit_coupling_map, read_results_directory, generate_all_statistics
 import qufi
 
 #%%
@@ -14,20 +14,13 @@ angles = {'theta0':[1, 2], 'phi0':[2, 3], 'theta1':[3, 4], 'phi1':[4, 5]}
 
 #%%
 device_backend = FakeSantiago()
-coupling_map = qufi.get_qiskit_coupling_map(circuits[0][0], device_backend)
+coupling_map = get_qiskit_coupling_map(circuits[0][0], device_backend)
 
 #%%
 results_names = execute_over_range(circuits, angles, coupling_map=coupling_map, results_folder="./tmp/")
 
 # %%
-results = qufi.read_results_directory("./tmp/")
-
-#%%
-print(results)
+results = read_results_directory("./tmp/")
 
 # %%
-qufi.compute_merged_histogram(results)
-qufi.compute_circuit_heatmaps(results)
-qufi.compute_circuit_delta_heatmaps(results)
-qufi.compute_qubit_histograms(results)
-qufi.compute_qubit_heatmaps(results)
+generate_all_statistics(results)
