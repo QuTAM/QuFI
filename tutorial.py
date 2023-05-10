31914d7bf190f7aed52b83f2c16ac584df0483a4
usesnames <emanuele.dri@studenti.polito.it>
2023-05-10 19:17:01 +0200

    removed useless parameters and imports

--- tutorial.py
+++ tutorial.py
@@ -1,10 +1,7 @@
 #%%
 import numpy as np
-import matplotlib
-# matplotlib.rcParams.update(matplotlib.rcParamsDefault)
-matplotlib.rcParams['text.usetex'] = True
 from qiskit.providers.fake_provider import FakeSantiago
-from qufi import execute_over_range_single, BernsteinVazirani, get_qiskit_coupling_map, read_file, generate_all_statistics
+from qufi import execute_over_range_single, BernsteinVazirani, get_qiskit_coupling_map, read_file
 
 #%%
 circuits = []
@@ -12,7 +9,7 @@ circuits = []
 bv4_p = BernsteinVazirani.build_circuit(3, '101')
 circuits.append((bv4_p, 'Bernstein-Vazirani_4'))
 
-angles = {'theta0':[0, np.pi/4], 'phi0':[0, np.pi/4], 'theta1':[0, 0], 'phi1':[0, 0]}
+angles = {'theta0':[0, np.pi/4], 'phi0':[0, np.pi/4]}
 
 #%%
 # device_backend = FakeSantiago()
